b5a0ea86cacb1b3467057d8106267c9c3281c5a3
冯梓杰 <fzijer@163.com>
2020-11-27 14:18:15 +0800

    add code

--- src/main/java/com/fzj/Stream/TestStreamApi.java
+++ src/main/java/com/fzj/Stream/TestStreamApi.java
@@ -2,11 +2,16 @@ package com.fzj.Stream;
 
 import com.fzj.annotation.Comment;
 import com.fzj.model.Artist;
+import com.fzj.model.Track;
 import com.sun.org.apache.xpath.internal.functions.Function;
 import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import lombok.val;
 import org.assertj.core.util.Lists;
 import org.assertj.core.util.Maps;
+import org.w3c.dom.ls.LSOutput;
 
+import javax.crypto.spec.PSource;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Vector;
@@ -52,7 +57,7 @@ public class TestStreamApi {
 
     /**
      * 5.常用流操作 collect( toList() )*/
-    List<String> collected = Stream.of("a", "b", "c")
+    List<String> collected = Stream.of("java", "scala", "oracle")
             .collect(Collectors.toList());
     collected.forEach(v -> {
       System.out.println(v);
@@ -75,8 +80,29 @@ public class TestStreamApi {
      * 7. fliter 操作 */
     System.out.println("=============filter============================filter==========================filter====================");
     Stream.of("a","b","c","java8").filter(v->v.equals("a")).forEach(v-> System.out.println(v));
+
+
+    /**
+     * 8.flatMap */
+    System.out.println("=============flatMap============================flatMap==========================flatMap====================");
+    Stream.of(collected,collected).flatMap(v ->{
+      return v.stream();
+    }).forEach(v-> System.out.println(v));
+
+    Stream.of(collected,collected).map(v->v.size()).forEach(v-> System.out.println(v));
+
+    /**
+     * 9.max & min */
+    System.out.println("=============max============================min==========================max====================");
+    List<Track> trackList =Lists.newArrayList(new Track("Bakal",524),new Track("Violets for your Furs",378),new Track("Time Was",451));
+    val terst=trackList.stream().min(Comparator.comparing(v->v.getNum())).get();
+    System.out.println(terst);
+
+
+
+
+
+
+
   }
-
-
-
 }
--- src/main/java/com/fzj/model/Track.java
+++ src/main/java/com/fzj/model/Track.java
@@ -1,7 +1,9 @@
 package com.fzj.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+@AllArgsConstructor
 @Data
 public class Track {
 
@@ -9,5 +11,7 @@ public class Track {
    *  曲目名称
    */
   private String name;
+
+  private long num;
 
 }
